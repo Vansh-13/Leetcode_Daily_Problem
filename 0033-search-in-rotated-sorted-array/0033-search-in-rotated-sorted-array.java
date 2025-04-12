1ff1203c8198717ce40844f32f3664e0c48d047a
Vansh Madaan <118126941+Vansh-13@users.noreply.github.com>
2025-04-12 16:07:26 +0530

    Time: 0 ms (100%), Space: 42.4 MB (19.78%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,41 @@
+class Solution {
+
+    public int findViot(int nums[]) {
+        int i = 0;
+        int j = nums.length - 1;
+        while (i < j) {
+            int mid = i + (j - i) / 2;
+            if (nums[mid] > nums[j]) {
+                i = mid + 1;
+            } else {
+                j = mid;
+            }
+        }
+        return i;
+    }
+
+    public int b(int i, int j, int nums[], int target) {
+        while (i <=j) {
+            int mid = i + (j - i) / 2;
+            if (nums[mid] > target) {
+                j = mid - 1;
+            } else if (nums[mid] < target) {
+                i = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int pivot = findViot(nums);
+        int idx = b(0, pivot - 1,nums,target);
+        if (idx != -1) {
+            return idx;
+        }
+        idx = b(pivot, n - 1,nums,target);
+        return idx;
+    }
+}
