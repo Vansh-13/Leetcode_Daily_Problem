09ce64569e5b07343d8f0c868b9f53e6ac065af3
Vansh Madaan <118126941+Vansh-13@users.noreply.github.com>
2025-04-14 20:56:26 +0530

    Time: 10 ms (77.35%), Space: 41.4 MB (45.6%) - LeetHub

--- 1656-count-good-triplets/1656-count-good-triplets.java
+++ 1656-count-good-triplets/1656-count-good-triplets.java
@@ -2,18 +2,20 @@ class Solution {
 
     public int countGoodTriplets(int[] arr, int a, int b, int c) {
         int co = 0;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                for (int k = j + 1; k < arr.length; k++) {
+        for (int i = 0; i <=arr.length-3; i++) {
+            for (int j = i + 1; j <=arr.length-2; j++) {
+                if(Math.abs(arr[i]-arr[j])<=a){
+                for (int k = j + 1; k <=arr.length-1; k++) {
                     int x = arr[i];
                     int y = arr[j];
                     int z = arr[k];
-                    if (Math.abs(x - y) <= a && Math.abs(y - z) <= b && Math.abs(x - z) <= c) {
+                    if ( Math.abs(y - z) <= b && Math.abs(x - z) <= c) {
                         co++;
                     }
+                }
                 }
             }
         }
         return co;
-    }
+    }                                                                                          
 }
